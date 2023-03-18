69d1257cb2448a2ad8e18aa8f18839fe63363328
bymoen <alexb@innotechcollege.com>
2023-03-18 10:47:56 -0600

    Made some while loops

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Initialize a counter to 0
+let counter = 0;
+// Check to see if the counter is less than or equal to 50
+while(counter <= 50) {
+    // Log the counter to the console
+    console.log(counter);
+    // Add 1 to the counter so eventually the loop stops
+    counter = counter + 1;
+}
+
+// Set the counter back to 0
+counter = 0;
+// Check to see if the counter is greater than or equal to -50
+while(counter >= -50) {
+    // Log the counter to the console
+    console.log(counter);
+    // Subtract 1 from the counter so eventually the loop stops
+    counter = counter - 1;
+}
+
+// Set the counter back to 0
+counter = 0;
+// Check to see if the counter is less than or equal to 100
+while(counter <= 100) {
+    // Log the counter to the console
+    console.log(counter);
+    // Add 2 to the counter so we only see even numbers and eventually the loop stops
+    counter = counter + 2;
+}
